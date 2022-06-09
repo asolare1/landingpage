edd668a490ed110f94444ec087735116ea52de6c
asolare1 <asolare1@gmail.com>
2022-06-09 00:52:40 +0000

    me

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer class="py-3 my-4 bg-dark mb-0">
+          <p class="text-center text-muted">© 2021 Company, Inc</p>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,25 @@
 import React from "react";
+import NavBar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Cards from "./cards.jsx";
+import Footer from "./footer.jsx";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<React.Fragment>
+			<NavBar></NavBar>
+			<Jumbotron></Jumbotron>
+			<Cards></Cards>
+			<Footer></Footer>
+		</React.Fragment>
+
+
 	);
 };
 
